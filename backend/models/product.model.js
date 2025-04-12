00f7ae7cde539fa7e5fcd46642a37431f75d3ae8
kiruthic <kiruthic.ptn@zohocorp.com>
2025-04-13 00:08:12 +0530

    basic CRUD operations method added

---
+++ backend/models/product.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema); //it will get the products collection
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 // const express = require('express'); Old way
 
 import express from "express";
-import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+import productRoutes from "./routes/product.route.js";
+dotenv.config();
 
 const app = express();
-
-dotenv.config();
 
 app.get("/", (req, res) =>{
     res.send("Server is Ready");
 });
 
+app.use(express.json()); //allows us to accept json data in req.body
 
-app.listen(5001, () => {
+app.use("/api/products", productRoutes);
+
+app.listen(5002, () => {
     connectDB();
     console.log("Server started ");
+
 });
 
